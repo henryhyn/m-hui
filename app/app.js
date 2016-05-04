80f9a7a8d6a10180f06c4836c7b24a20d241e451
何永能 <yongneng.he@dianping.com>
2016-05-05 02:21:10 +0800

    Home

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-import { Grids } from './index';
+import Home from './pages/home/Home';
 
 class App extends Component {
     render() {
@@ -16,7 +16,7 @@ class App extends Component {
 ReactDOM.render((
     <Router history={browserHistory}>
         <Route path="/" component={App}>
-            <IndexRoute component={Grids}/>
+            <IndexRoute component={Home}/>
         </Route>
     </Router>
 ), document.getElementById('container'));
---
+++ app/pages/home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+import classNames from 'classnames';
+import { Grids } from '../../index';
+
+class Home extends Component {
+    render() {
+        return (
+            <Grids />
+        );
+    }
+}
+
+Home.propTypes = {};
+Home.defaultProps = {};
+
+export default Home;
