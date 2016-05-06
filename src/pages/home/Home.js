c4cd268c5de5c0ba10dd421136e93d6553884cfa
何永能 <yongneng.he@dianping.com>
2016-05-06 10:36:01 +0800

    home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -15,6 +15,13 @@ import IconTab from './icon_nav_tab.png';
 import IconSearchBar from './icon_nav_search_bar.png';
 import IconRefresh from './icon_nav_ptr.png';
 import IconScroll from './icon_nav_up.png';
+import IconCalendar from './icon_nav_calendar.png';
+import IconCity from './icon_nav_city.png';
+import IconDateTime from './icon_nav_datetime.png';
+import IconNotify from './icon_nav_noti.png';
+import IconPicker from './icon_nav_picker.png';
+import IconSelect from './icon_nav_select.png';
+import IconSwiper from './icon_nav_swiper.png';
 
 class Home extends Component {
     constructor(props) {
@@ -22,23 +29,27 @@ class Home extends Component {
         this.state = {
             components: [{
                 icon: <img src={IconButton}/>,
-                label: 'Button',
+                label: '按钮',
                 href: 'button'
             }, {
                 icon: <img src={IconCell}/>,
                 label: 'Cell',
                 href: 'cell'
             }, {
+                icon: <img src={IconCell}/>,
+                label: '表单',
+                href: 'form'
+            }, {
                 icon: <img src={IconToast}/>,
                 label: 'Toast',
                 href: 'toast'
             }, {
                 icon: <img src={IconDialog}/>,
-                label: 'Dialog',
+                label: '对话框',
                 href: 'dialog'
             }, {
                 icon: <img src={IconProgress}/>,
-                label: 'Progress',
+                label: '进度条',
                 href: 'progress'
             }, {
                 icon: <img src={IconMsg}/>,
@@ -46,7 +57,7 @@ class Home extends Component {
                 href: 'msg'
             }, {
                 icon: <img src={IconArticle}/>,
-                label: 'Article',
+                label: '文章',
                 href: 'article'
             }, {
                 icon: <img src={IconActionSheet}/>,
@@ -58,7 +69,7 @@ class Home extends Component {
                 href: 'icons'
             }, {
                 icon: <img src={IconPanel}/>,
-                label: 'Panel',
+                label: '面板',
                 href: 'panel'
             }, {
                 icon: <img src={IconTab}/>,
@@ -66,27 +77,55 @@ class Home extends Component {
                 href: 'tab'
             }, {
                 icon: <img src={IconSearchBar}/>,
-                label: 'SearchBar',
+                label: '搜索框',
                 href: 'searchbar'
             }, {
                 icon: <img src={IconRefresh}/>,
-                label: 'Refresh',
+                label: '下拉刷新',
                 href: 'refresh'
             }, {
                 icon: <img src={IconScroll}/>,
-                label: 'Scroll',
+                label: '滚动加载',
                 href: 'scroll'
             }, {
-                icon: <img src={IconSearchBar}/>,
+                icon: <img src={IconPanel}/>,
                 label: 'Popup',
                 href: 'popup'
+            }, {
+                icon: <img src={IconCalendar}/>,
+                label: '日历',
+                href: 'Calendar'
+            }, {
+                icon: <img src={IconCity}/>,
+                label: '城市',
+                href: 'city'
+            }, {
+                icon: <img src={IconDateTime}/>,
+                label: '日期时间',
+                href: 'datetime'
+            }, {
+                icon: <img src={IconNotify}/>,
+                label: '通知',
+                href: 'notify'
+            }, {
+                icon: <img src={IconPicker}/>,
+                label: 'Picker',
+                href: 'picker'
+            }, {
+                icon: <img src={IconSelect}/>,
+                label: 'Select',
+                href: 'select'
+            }, {
+                icon: <img src={IconSwiper}/>,
+                label: '轮播',
+                href: 'swiper'
             }]
         };
     }
 
     render() {
         return (
-            <Page title='九宫格' subTitle='可用于导航栏'>
+            <Page title='HUI Mobile' subTitle='基于 React 开发的移动端轻量 UI 库'>
                 <Grids data={this.state.components}/>
             </Page>
         );
