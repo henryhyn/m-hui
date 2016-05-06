1c6e5f398e0e0a95d68a038305675daad9f46b0d
何永能 <yongneng.he@dianping.com>
2016-05-06 14:18:18 +0800

    add files

---
+++ src/pages/navbar/NavBarDemo.js
@@ -0,0 +1,21 @@
+import React, { Component, PropTypes } from 'react';
+import classNames from 'classnames';
+
+class NavBarDemo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>开发中, 敬请期待...</div>
+        );
+    }
+}
+
+NavBarDemo.propTypes = {};
+
+NavBarDemo.defaultProps = {};
+
+export default NavBarDemo;
