9212865e8e76912100536f2846ce045e92152564
何永能 <yongneng.he@dianping.com>
2016-05-07 20:58:45 +0800

    import all

--- src/index.js
+++ src/index.js
@@ -1,47 +1,114 @@
 require('./less/style.less');
 import Page from './components/page/Page';
-import Grids from './components/grid/Grids';
-import Grid from './components/grid/Grid';
-import GridIcon from './components/grid/GridIcon';
-import GridLabel from './components/grid/GridLabel';
+import ActionSheet from './components/actionsheet/ActionSheet';
+import Article from './components/article/Article';
 import Button from './components/button/Button';
 import ButtonArea from './components/button/ButtonArea';
-import Icon from './components/icon/Icon';
-import Toast from './components/toast/Toast';
-import Mask from './components/mask/Mask';
 import Cell from './components/cell/Cell';
-import CellHeader from './components/cell/CellHeader';
 import CellBody from './components/cell/CellBody';
 import CellFooter from './components/cell/CellFooter';
+import CellHeader from './components/cell/CellHeader';
 import Cells from './components/cell/Cells';
-import CellsTitle from './components/cell/CellsTitle';
 import CellsTips from './components/cell/CellsTips';
+import CellsTitle from './components/cell/CellsTitle';
+import Alert from './components/dialog/Alert';
+import Confirm from './components/dialog/Confirm';
+import Checkbox from './components/form/Checkbox';
 import Form from './components/form/Form';
 import FormCell from './components/form/FormCell';
-import Checkbox from './components/form/Checkbox';
 import Input from './components/form/Input';
 import Radio from './components/form/Radio';
 import Select from './components/form/Select';
 import Switch from './components/form/Switch';
 import TextArea from './components/form/TextArea';
 import Uploader from './components/form/Uploader';
+import Grid from './components/grid/Grid';
+import GridIcon from './components/grid/GridIcon';
+import GridLabel from './components/grid/GridLabel';
+import Grids from './components/grid/Grids';
+import Icon from './components/icon/Icon';
+import Label from './components/label/Label';
+import Mask from './components/mask/Mask';
+import MediaBox from './components/mediabox/MediaBox';
+import MediaBoxBody from './components/mediabox/MediaBoxBody';
+import MediaBoxDescription from './components/mediabox/MediaBoxDescription';
+import MediaBoxHeader from './components/mediabox/MediaBoxHeader';
+import MediaBoxInfo from './components/mediabox/MediaBoxInfo';
+import MediaBoxInfoMeta from './components/mediabox/MediaBoxInfoMeta';
+import MediaBoxTitle from './components/mediabox/MediaBoxTitle';
+import Msg from './components/msg/Msg';
+import Panel from './components/panel/Panel';
+import PanelBody from './components/panel/PanelBody';
+import PanelFooter from './components/panel/PanelFooter';
+import PanelHeader from './components/panel/PanelHeader';
+import Popup from './components/popup/Popup';
+import Progress from './components/progress/Progress';
+import SearchBar from './components/searchbar/SearchBar';
+import NavBar from './components/tab/NavBar';
+import NavBarItem from './components/tab/NavBarItem';
+import Tab from './components/tab/Tab';
+import TabBar from './components/tab/TabBar';
+import TabBarIcon from './components/tab/TabBarIcon';
+import TabBarItem from './components/tab/TabBarItem';
+import TabBarLabel from './components/tab/TabBarLabel';
+import TabBody from './components/tab/TabBody';
+import TabBodyItem from './components/tab/TabBodyItem';
+import Toast from './components/toast/Toast';
 
 export {
     Page
-    , Grids
+    , ActionSheet
+    , Article
+    , Button
+    , ButtonArea
+    , Cell
+    , CellBody
+    , CellFooter
+    , CellHeader
+    , Cells
+    , CellsTips
+    , CellsTitle
+    , Alert
+    , Confirm
+    , Checkbox
+    , Form
+    , FormCell
+    , Input
+    , Radio
+    , Select
+    , Switch
+    , TextArea
+    , Uploader
     , Grid
     , GridIcon
     , GridLabel
-    , Button
-    , ButtonArea
+    , Grids
     , Icon
-    , Toast
+    , Label
     , Mask
-    , Cell
-    , CellHeader
-    , CellBody
-    , CellFooter
-    , Cells
-    , CellsTitle
-    , CellsTips
+    , MediaBox
+    , MediaBoxBody
+    , MediaBoxDescription
+    , MediaBoxHeader
+    , MediaBoxInfo
+    , MediaBoxInfoMeta
+    , MediaBoxTitle
+    , Msg
+    , Panel
+    , PanelBody
+    , PanelFooter
+    , PanelHeader
+    , Popup
+    , Progress
+    , SearchBar
+    , NavBar
+    , NavBarItem
+    , Tab
+    , TabBar
+    , TabBarIcon
+    , TabBarItem
+    , TabBarLabel
+    , TabBody
+    , TabBodyItem
+    , Toast
 };
